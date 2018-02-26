20ca499b05d600ed7096380961c734a5ac8f2529
Kate Makarenka <polumna-la@mail.ru>
2018-02-26 15:42:46 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let [a,b,c] = equation.replace(/x\^2| /gi,'').match(/-?\d+/gi)
+  
+  let disc = (b * b) - 4 * (a * c)
+  
+  return [Math.round((-b + Math.sqrt(disc)) / (2 * a)),Math.round((-b - Math.sqrt(disc)) / (2 * a))].sort((a,b)=> a-b)
 }
